8adefb20496710c60f6d942c83d951ba16043f74
Đỗ Thế Đức <ducdtth2108036@fpt.edu.vn>
2023-09-19 23:11:30 +0700

    delete

--- src/main/java/com/example/freshfoodapi/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.freshfoodapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetailRepository,Long>, JpaSpecificationExecutor {
-}
--- src/main/java/com/example/freshfoodapi/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.freshfoodapi.service;
-
-import com.example.freshfoodapi.dto.RestErrorDto;
-import com.example.freshfoodapi.entity.Category;
-
-public interface CategoryService {
-
-
-}
