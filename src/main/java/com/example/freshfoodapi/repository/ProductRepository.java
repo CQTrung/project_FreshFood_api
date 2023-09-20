13e1eca3913c85d0a622335faeb015f891c38112
CQTrung <ctrung6123@gmail.com>
2023-09-20 10:44:57 +0700

    update

--- src/main/java/com/example/freshfoodapi/repository/ProductRepository.java
+++ src/main/java/com/example/freshfoodapi/repository/ProductRepository.java
@@ -1,14 +1,9 @@
 package com.example.freshfoodapi.repository;
 
-
-import com.example.freshfoodapi.dto.ProductDto;
 import com.example.freshfoodapi.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor {
